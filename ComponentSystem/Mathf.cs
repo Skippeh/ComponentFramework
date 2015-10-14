68ade9b6ae9aa09d4ab66007b4455acbb883bb04
Jonathan Lindahl <jonathanjl99@gmail.com>
2015-10-14 21:40:42 +0200

    Added some math helpers and Mathf test.

--- ComponentSystem/Mathf.cs
+++ ComponentSystem/Mathf.cs
@@ -8,6 +8,8 @@ namespace ComponentSystem
         public static float PI => (float) Math.PI;
         public static float E => (float) Math.E;
 
+        private static readonly Random random = new Random();
+
         /// <summary>Smoothly interpolates the vector between 0-1-0.</summary>
         public static Vector2 SmoothLerp010(Vector2 a, Vector2 b, float amount)
         {
@@ -19,6 +21,32 @@ namespace ComponentSystem
         {
             var val = (float) (Math.Abs(Math.Sin(amount * PI)));
             return a + (b - a) * val;
+        }
+
+        /// <summary>Gets a random float between min and max.</summary>
+        public static float Random(float min = 0f, float max = 1f)
+        {
+            return (float) (min + ((max - min) * random.NextDouble()));
+        }
+
+        /// <summary>Gets a random Vector2 between min and max.</summary>
+        public static Vector2 Random(Vector2 min, Vector2 max)
+        {
+            return new Vector2(Random(min.X, max.X), Random(min.Y, max.Y));
+        }
+
+        /// <summary>Gets a random point around vector within the specified radius.</summary>
+        public static Vector2 Random(Vector2 vector, float radius)
+        {
+            var angle = (float) random.NextDouble() * MathHelper.TwoPi;
+            var direction = new Vector2((float) Math.Cos(angle), (float) Math.Sin(angle));
+
+            return direction * (float) random.NextDouble() * radius;
+        }
+
+        public static Vector2 ToDirection(float radians)
+        {
+            return new Vector2((float) Math.Sin(radians), (float) Math.Cos(radians));
         }
     }
 }
---
+++ Tests/Tests/MathfTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ComponentSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+
+namespace Tests.Tests
+{
+    [TestClass]
+    public class MathfTest
+    {
+        [TestMethod]
+        public void TestMathf()
+        {
+            // Test Mathf.ToDirection
+            Vector2 direction = Mathf.ToDirection(MathHelper.ToRadians(90));
+            Assert.IsTrue(Math.Abs(direction.X - 1d) <= 0.0001d && Math.Abs(direction.Y) <= 0.0001d, "ToDirection test expected Vec2(1, 0).");
+            
+            // Test Mathf.SmoothLerp010
+            Assert.AreEqual(2f, Mathf.SmoothLerp010(1f, 2f, 0.5f), "SmoothLerp010(1, 2, 0.5) expected 2.");
+        }
+    }
+}
